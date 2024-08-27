a0f3c0621033a9b34c7e36c2ae1e31ec8beffa34
Raghava Rao Arrebelly <raghavaarrebelly@Raghavas-MBP.lan>
2024-08-27 12:12:19 -0400

    fixing the hyperlink

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const Footer = () => (
   <footer className="fixed bottom-0 left-0 w-full bg-gray-800 text-white text-center py-4">
-    &copy; UpUnikSelf &nbsp &nbsp <a href="https://github.com/raoraghava/UpunikSelf-Assesment">Visit Source Code!</a>
-
+    &copy; UpUnikSelf{" "} || 
+    <a href="https://github.com/raoraghava/UpunikSelf-Assesment">
+      Visit Source Code!
+    </a>
   </footer>
 );
 
