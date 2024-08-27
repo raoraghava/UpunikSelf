4a5b07b3048d50a5cbfc6ce482a7de9ceef5b6fd
Raghava Rao Arrebelly <raghavaarrebelly@Raghavas-MBP.lan>
2024-08-27 12:02:32 -0400

    After adding source code hyperlink

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,8 @@ import React from "react";
 
 const Footer = () => (
   <footer className="fixed bottom-0 left-0 w-full bg-gray-800 text-white text-center py-4">
-    &copy; UpUnikSelf
+    &copy; UpUnikSelf &nbsp &nbsp <a href="https://github.com/raoraghava/UpunikSelf-Assesment">Visit Source Code!</a>
+
   </footer>
 );
 
